2367f10692d09abac8f7e762cae44ffb2975b5e6
Reehan Sarmah <94010840+Reehan09Sarmah@users.noreply.github.com>
2024-11-09 17:15:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,6 +83,7 @@ clf.fit(encoded_penguin_data_Xtrain, encoded_y)
 prediction = clf.predict(encoded_input_penguin)
 prediction_proba = clf.predict_proba(encoded_input_penguin)
 
+prediction
 prediction_proba
 
 

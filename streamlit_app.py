4d4a6b89b30cd1805f4e0bf5ea85bdc61aaa2900
Reehan Sarmah <94010840+Reehan09Sarmah@users.noreply.github.com>
2024-11-09 11:39:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,28 @@ with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
 # Data Preparations
-with st.sidebar: # To let users create a data point of their own
+
+# To let users create a data point of their own and add to the existing data
+with st.sidebar: 
   st.header('Input Features')
   # "island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
   island = st.selectbox('Island',('Torgersen', 'Biscoe', 'Dream'))
-  gender = st.selectbox('Gender', ('male', 'female'))
   bill_length_mm = st.slider('Bill Length(mm)', min_value=32.1, max_value=59.6, value=43.9)
+  bill_depth_mm = st.slider('Bill Depth(mm)', min_value=13.1, max_value=21.5, value=17.2)
+  flipper_length_mm = st.slider('Flipper Length(mm)', min_value=172.0, max_value=231.0, value=201.0)
+  body_mass_g = st.slider('Body Mass(g)', min_value=2700.0, max_value=6300.0, value=4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
+  
+  # create a dataframe
+  data = {'island': island,
+         'bill_length_mm': bill_length_mm,
+         'bill_depth_mm': bill_depth_mm,
+         'flipper_length_mm': flipper_length_mm,
+         'body_mass_g': body_mass_g,
+         'gender': gender}
+
+  input_df = pd.DataFrame[data, index=[0])
+
+input_df
+  
   
